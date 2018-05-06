9a2fdcd2792f1a47daf53767ea8d1144c677847e
AntonioMtnz <AntonioMtnz@ANTONIOMTNZ-HP>
2018-05-06 14:54:47 -0500

    Agregar archivos de proyecto.

---
+++ AutoCompletar/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace AutoCompletar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.eHTxtCiudad = new System.Windows.Forms.Integration.ElementHost();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.picIcon = new System.Windows.Forms.PictureBox();
+            this.lblCliente = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // eHTxtCiudad
+            // 
+            this.eHTxtCiudad.BackColor = System.Drawing.Color.White;
+            this.eHTxtCiudad.Location = new System.Drawing.Point(26, 106);
+            this.eHTxtCiudad.Name = "eHTxtCiudad";
+            this.eHTxtCiudad.Size = new System.Drawing.Size(235, 36);
+            this.eHTxtCiudad.TabIndex = 3;
+            this.eHTxtCiudad.Child = null;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Segoe UI", 13F);
+            this.lblNombre.Location = new System.Drawing.Point(21, 78);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(72, 25);
+            this.lblNombre.TabIndex = 2;
+            this.lblNombre.Text = "Ciudad:";
+            // 
+            // picIcon
+            // 
+            this.picIcon.BackColor = System.Drawing.Color.White;
+            this.picIcon.Image = global::AutoCompletar.Properties.Resources.lupa;
+            this.picIcon.Location = new System.Drawing.Point(229, 109);
+            this.picIcon.Name = "picIcon";
+            this.picIcon.Size = new System.Drawing.Size(30, 30);
+            this.picIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picIcon.TabIndex = 3;
+            this.picIcon.TabStop = false;
+            // 
+            // lblCliente
+            // 
+            this.lblCliente.AutoSize = true;
+            this.lblCliente.Font = new System.Drawing.Font("Segoe UI", 13F);
+            this.lblCliente.Location = new System.Drawing.Point(21, 9);
+            this.lblCliente.Name = "lblCliente";
+            this.lblCliente.Size = new System.Drawing.Size(82, 25);
+            this.lblCliente.TabIndex = 0;
+            this.lblCliente.Text = "Nombre:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.txtNombre.Location = new System.Drawing.Point(26, 37);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(235, 32);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(186, 165);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 45);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Guardar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(286, 238);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblCliente);
+            this.Controls.Add(this.picIcon);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.eHTxtCiudad);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Registros";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picIcon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Integration.ElementHost eHTxtCiudad;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.PictureBox picIcon;
+        private System.Windows.Forms.Label lblCliente;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ AutoCompletar/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using System.Windows.Media;
+
+namespace AutoCompletar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            CargarAutoComplete();
+        }
+
+        //Método para cargar la lista de autocompletado.
+        private void CargarAutoComplete()
+        {
+            AutoCompleteBox aList = new AutoCompleteBox();
+            aList.FontSize = 16;
+            aList.FontFamily = new FontFamily("Segoe UI");
+            //u.IsTextCompletionEnabled = true;
+            aList.FilterMode = AutoCompleteFilterMode.Contains;
+           
+            List<string> cityList = new List<string>(){ "Guadalajara",
+                                                "Guanajuato",
+                                                "Guaymas",
+                                                "Puebla",
+                                                "Morelia",
+                                                "Monterrey",
+                                                "Querétaro",
+                                                "Mérida",
+                                                "CDMX",
+                                                "Xalapa",
+                                                "Zacatecas" };
+
+            aList.MinimumPopulateDelay = 100;
+            aList.MinimumPrefixLength = 1;
+            aList.ItemsSource = cityList;
+
+            eHTxtCiudad.Child = aList;
+        }
+    }
+}
